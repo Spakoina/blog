3f66c501d4801daf38f7f68218766c1055a7aafe
Elia Rebellato <reb.sennare@gmail.com>
2022-01-07 21:07:38 +0100

    Aggiunto link alla lista di articoli

--- views/search.php
+++ views/search.php
@@ -18,12 +18,17 @@ foreach ($articles as $key => $value) {
     <article class="blog-post row">
         <div class="col-5">
             <?php if (strlen($value->photo) > 0) { ?>
-                <img class="img-fluid" 
-                     src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/<?php echo $value->photo; ?>">
-                 <?php } ?>
+                <a href="<?php echo $GLOBALS['base_complete_url'] . '/article/' . $value->id_article_url_cd; ?>">
+                    <img class="img-fluid" 
+                         src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/<?php echo $value->photo; ?>">
+                </a>
+            <?php } ?>
         </div>
         <div class="col-7">
-            <h2 class="blog-post-title"><?php echo $value->title; ?></h2>
+            <a class="article-title-link"
+               href="<?php echo $GLOBALS['base_complete_url'] . '/article/' . $value->id_article_url_cd; ?>">
+                <h2 class="blog-post-title"><?php echo $value->title; ?></h2>
+            </a>
             <p class="blog-post-meta fst-italic"><?php echo format_date(strtotime($value->date)); ?></p>
 
             <p>
