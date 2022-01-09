52b83de366ef2e36139a46cfbad29327d2563f14
Elia Rebellato <reb.sennare@gmail.com>
2022-01-09 21:14:22 +0100

    Immagini ottimizzate e rimosse
    correzione right menu

--- templates/right-menu.php
+++ templates/right-menu.php
@@ -1,7 +1,7 @@
 
 <div style="top: 2rem;">
-    <div class="p-4 mb-3 bg-light rounded">
-        <img class="img-fluid rounded-circle" src="<?php echo $GLOBALS['base_complete_url']; ?>/img/scrivere.jpg">
+    <div class="p-4 mb-3 bg-light rounded text-center">
+        <img class="img-fluid rounded-circle w-75 mx-auto d-block" src="<?php echo $GLOBALS['base_complete_url']; ?>/img/scrivere.jpg">
         <h4 class="fst-italic">About</h4>
         <p class="mb-0">Ehilà curiosa o curioso!<br><!-- comment --> 
             Sono Chiara e sono molto felice tu sia su questo blog.<br><!-- comment -->
