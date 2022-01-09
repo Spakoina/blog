fa4ca2d6bc8c93088fcbc28450e1762fe1fdf50d
Chiara Censorio <chiaracens@gmail.com>
2022-01-09 16:46:48 +0100

    Ko fi aggiunto al menu di destra

--- templates/right-menu.php
+++ templates/right-menu.php
@@ -1,5 +1,5 @@
 
-<div style="top: 2rem;">
+<div class="position-sticky" style="top: 2rem;">
     <div class="p-4 mb-3 bg-light rounded">
         <img class="img-fluid rounded-circle" src="<?php echo $GLOBALS['base_complete_url']; ?>/img/scrivere.jpg">
         <h4 class="fst-italic">About</h4>
@@ -8,11 +8,9 @@
             Leggo soprattutto saggi e narrativa e mi piace condividere tutto quello che scopro e che mi fa crescere. <br><!-- comment -->
             Spero di aiutarti a fare lo stesso.  </p>
     </div>
-    
+
     <div class="text-center">
         Mi trovi anche qui 
-       
-         
-        
     </div>
+    <iframe id='kofiframe' src='https://ko-fi.com/papergirlblog/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='712' title='papergirlblog'></iframe>
 </div>
