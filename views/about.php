4161972004a9bb9fcda1946dd2680461c655361d
Chiara Censorio <chiaracens@gmail.com>
2022-01-24 00:29:49 +0100

    cose

--- views/about.php
+++ views/about.php
@@ -17,13 +17,14 @@
     <h2 class="text-spacing-2">SU DI ME</h2>
     <br>
     Sono Chiara, sono nata negli anni ‘90 e
-    dal Tamagotchi ai libri è stato un attimo grazie a ”Il Piccolo Principe” regalatomi da mia madre. <br>
+    dal Tamagotchi ai libri è stato un attimo grazie a ”Il Piccolo Principe” regalatomi da mia madre quando ero piccola. <br>
     Volendo scoprire cosa ci fosse oltre le montagne abruzzesi ho approfittato della fine del liceo per studiare in un’altra regione. 
     <br>
     Lì mi sono dilettata all’università fra romanzi di Dostojevskij e Machado de Assis, studiando russo e portoghese sorseggiando spritz all’ombra dei campanili di Venezia.
     <br>
-    ___<br><br>
-    Puoi venire a farmi un saluto Instagram <a class="text-spacing-2 article-title-link" href="https://www.instagram.com/papergirl.ninja/" target="_blank">@papergirl.ninja</a> 
+    Volevo un posto tutto mio per scrivere e condividere le mie impressioni sulle letture ed eccomi qua su questo blog.
+    <br>___<br><br>
+    Puoi venire a farmi un saluto su Instagram <a class="text-spacing-2 article-title-link" href="https://www.instagram.com/papergirl.ninja/" target="_blank">@papergirl.ninja</a> 
     o su <a class="text-spacing-2 article-title-link" href="https://www.goodreads.com/user/show/104782651-evadida-eu-sou" target="_blank">Goodreads</a>. 
     Oppure puoi scrivermi un’email <a class="text-spacing-2 article-title-link"  href="mailto:paperblogchiara@gmail.com">(paperblogchiara@gmail.com)</b></a>, rispondo sempre.
 
@@ -36,12 +37,9 @@
 La storia inizia con un account Instagram creato 
 all’inizio del 2021 <a class="text-spacing-2 article-title-link" href="https://www.instagram.com/papergirl.ninja/" target="_blank">@papergirl.ninja</a> .
 <br>
-Lo spazio concesso dai post di Instagram è diventato però troppo limitante. <br>
-Alla fine la piattaforma è fatta più che altro per condividere foto e quindi 
+Lo spazio concesso dai post di Instagram però è diventato presto troppo limitante: <br>
+alla fine la piattaforma è fatta più che altro per condividere foto e quindi 
 per poter scrivere con più libertà decido nel 2022 di crearmi un blog.
-<br><br>
-Non volevo utilizzare una piattaforma con layout prestabiliti, perciò ho creato un sito da zero, 
-dalla pagina Web bianca con codici html, partendo da zero come quando si inizia a scrivere su un foglio.
 <br><br>
 Spero ti piaccia sfogliare le pagine di <em>Papergirl Blog</em>. <br> A me ha divertito tantissimo crearlo e non vedo l’ora 
 di scrivere qui appena finita una lettura per aggiornarlo. Spero ti ispiri in qualche 
