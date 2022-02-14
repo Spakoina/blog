e843b1ae82ef8368659d983e804a876fe6446393
Chiara Censorio <chiaracens@gmail.com>
2022-02-14 15:04:36 +0100

    cose

--- views/articles/libro-bill-gates-clima-come-evitare-un-disastro.php
+++ views/articles/libro-bill-gates-clima-come-evitare-un-disastro.php
@@ -11,14 +11,14 @@
 
     <div class="row">
         <div class="col justify-text">
-<h2>Un libro sulla questione climatica</h2>
+            <h2>Un libro sulla questione climatica</h2><br>
 Ma Chiara, tu che stai sempre a leggere saggi, un libro per fare il punto della questione climatica non pesante?
 <br><br>
 Questo qui. Clima. Come evitare un disastro di Bill Gates.
 <br><br>
 Ho iniziato dall’anno scorso a leggere libri che avessero come tematica quella dell’ambiente, 
 per informarmi e per adottare comportamenti più sostenibili, con ahimè, scarsi risultati (per ora). 
-Fra quei pochi testi che ho letto questo è il più <b>completo e aggiornato e sincero</b> perché porsi il fine 
+Fra quei pochi testi che ho letto questo è il più <b>completo, aggiornato e sincero</b>: porsi il fine 
 di limitare semplicemente le emissioni non basterà, l’unico traguardo ragionevole sono le emissioni zero entro il 2050. 
 Inoltre mi piace perché è <b>ottimista</b>: "Abbiamo bisogno di incanalare la passione e il QI scientifico del mondo per sfruttare 
 al meglio le soluzioni che abbiamo già per produrre l’energia pulita e inventarne di nuove". 
@@ -26,15 +26,15 @@ al meglio le soluzioni che abbiamo già per produrre l’energia pulita e invent
 <br><br>
 
 
-<h2>Il punto forte del libro</h2>
+<h2>Il punto forte del libro</h2><br>
 Il tono dell’autore è quello di un ingegnere 
 più che di un politologo; Gates infatti dice chiaramente di non avere una soluzione politica, ma spera di 
 <b>focalizzare il dibattito sui passi necessari per azzerare le emissioni</b>. Il modo di comunicare le idee è uno 
 degli elementi rilevanti e vincenti di questo libro: il testo è diviso in capitoli e il linguaggio è chiarissimo, 
-le frasi sono brevi e semplici e, anche quando Gates si dilunga con qualche dato in più, <b>il libro rimane sempre scorrevolissimo e chiaro</b>.
+le frasi sono brevi e semplici e, anche quando Gates si dilunga con qualche dato in più, <b>il libro rimane sempre scorrevolissimo e chiaro.</b>.
 <br><br>
 
-<h2>Il messaggio fondamentale</h2>
+<h2>Il messaggio fondamentale</h2><br>
 I primi capitoli inquadrano il problema dell’inquinamento e le previsioni su quello che potrebbe accadere. 
 Sappiamo tante cose sui cambiamenti in atto adesso e su quello che è verificato negli ultimi secoli o millenni, 
 ma è anche vero che molte cose i climatologi non possono prevederle con certezza. Le reali ripercussioni dell’inquinamento 
@@ -42,24 +42,25 @@ sull’ambiente quali saranno? Al momento sono già milioni le persone costrette
 il cibo o costrette ad andare via, molte specie animali sono a rischio e l’equilibrio che permette la nostra esistenza 
 sulla Terra è precario, come era emerso anche dalla lettura del libro  
 <a class="article-title-link" href="hhttps://www.papergirlblog.com/meta-della-terra-edward-wilson" target="_blank">
-    <b>Metà della Terra di Edward Wilson sul quale ho scritto un articolo sul blog</b></a>
+    <b>Metà della Terra di Edward Wilson sul quale ho scritto un articolo sul blog.</b></a>
 <br><br>
 Sappiamo inoltre che il 40% delle emissioni globali vengono prodotte dal 16% più ricco della popolazione. 
-La bella notizia è che la parte ricca del mondo prospererà nei prossimi decenni: sempre più nazioni vedono 
+La bella notizia è che la parte ricca del mondo prospererà nei prossimi decenni: sempre più nazioni infatti vedono 
 aumentare il loro benessere, ma per avere buone infrastrutture, trasporti e tutto quello che serve per una 
 vita dignitosa, queste dovranno inquinare, così come ha fatto e continua a fare l’occidente 
 da oltre un secolo. L’autore non nega questo squilibrio e considera eticamente giusto che tutti abbiano
 la possibilità di prosperare e vedere migliorare le proprie condizioni di vita.
 <br><br>
 Gates vuole comunicare fondamentalmente che <b>l'obiettivo zero emissioni sarà difficilissimo da raggiungere</b>,
-ci vorrà un grande lavoro di convincimento per molti paesi e che la ricerca sarà fondamentale perché quello che si fa già non basta. 
+ci vorrà un grande lavoro di convincimento per molti paesi ancora restii ad accettare la situazione
+e che la ricerca sarà fondamentale perché quello che si fa già non basta. 
 Ricoprire la terra di alberi non è fattibile e non aiuterebbe comunque a ridurre o far assorbire l'inquinamento! 
 Inoltre, non è possibile utilizzare solo le fonti rinnovabili, ma ci vorrà un mix di tutto quello che si riuscirà a escogitare, compreso 
 il miglioramento delle tecnologie già esistenti.
 Per questo occorrono di nuovo sviluppo, ricerca e tanti finanziamenti.
 <br><br>
 
-<h2> Quali domande porsi? </h2>
+<h2> Quali domande porsi? </h2><br>
 La parte che non vedevo l'ora di leggere si trova verso la fine del libro e
 riguarda le domande da porsi o le accortezze da avere per destreggiarsi nelle conversazioni o nella 
 lettura di testi che riguardano la tematica ambientale. <br><br>
@@ -72,7 +73,7 @@ L’inquinamento non riguarda solo la plastica e vi sorprenderà leggere in quan
 l'ambiente in misura anche maggiore rispetto a quello che riguarda la sola plastica. <br><br>
 
 
- <div class="col-8 offset-2">
+ <div class="col-8 offset-2 mb-3">
 
                 <img class="img-fluid text-center" 
                      alt="Foto centrale" 
@@ -85,20 +86,23 @@ l'ambiente in misura anche maggiore rispetto a quello che riguarda la sola plast
             </div>
 
 
-<h2>Che cosa possiamo fare noi cittadini?</h2>
+<h2>Che cosa possiamo fare noi cittadini?</h2><br>
 L’ultima parte del libro è dedicata alle cose che possiamo fare già da oggi. <br><br>
 Le leggi e le norme ormai sono troppo datate, non elaborate tenendo conto del cambiamento climatico 
-(Gates si riferisce esplicitamente alle leggi statunitensi, ma non credo che in Italia sia stato fatto qualcosa di diverso). Un ambiamento della politica
+(Gates si riferisce esplicitamente alle leggi statunitensi, ma non credo che in Italia sia stato fatto qualcosa di diverso). 
+Un cambiamento della politica
 sarebbe quindi auspicabile. Cosa si può fare adesso in quanto consumatori, 
 in quanto cittadini e in quanto dipendenti o datori di lavoro? <br><br>
 
 Ecco qualche proposta:<br>
-scrivere lettere con richieste ai politici, frequentare i municipi, guardare alla propria realtà 
-locale oltre che a quella nazionale, provare hamburger vegetali e, per le aziende, adottare carbon tax interne. <br><br>
+scrivere lettere con richieste ai politici,<br> 
+frequentare i municipi, guardare alla propria realtà locale oltre che a quella nazionale,<br> 
+provare hamburger vegetali<br><!-- comment -->
+e, per le aziende, adottare carbon tax interne. <br>
 Oltre all'importanza della ricerca e dello sviluppo Gates evidenzia anche il ruolo di incentivi e misure fiscali come
 una tassa sull’anidride carbonica. <br><br>
 
-Una solzione da lui elaborata è l'adozione del cosiddetto Green Premium che spiega bene anche 
+Una soluzione da lui elaborata è l'adozione del cosiddetto Green Premium che spiega bene anche 
 <a class="article-title-link" href="hhttps://www.gatesnotes.com/Energy/Introducing-the-Green-Premiums" target="_blank"><b>qui su un articolo nel suo blog</b></a>
 che è sostanzialmente la differenza di costo tra un sistema di produzione che non inquina e uno che produce invece carbonio. <br><br>
 
