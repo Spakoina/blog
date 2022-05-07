266118bac9537bd851ab55f5584469be925aeb1b
Chiara Censorio <chiaracens@gmail.com>
2022-05-07 14:40:04 +0200

    Tabella Frasi quot Ucr

--- templates/right-menu.php
+++ templates/right-menu.php
@@ -2,7 +2,7 @@
     <div class="p-2 mb-5 bg-light rounded text-center">
         <img class="img-fluid rounded-circle mx-auto d-block" src="<?php echo $GLOBALS['base_complete_url']; ?>/img/chiara.jpg">
         <h4 class="fst-italic pt-2">Benvenuti sul mio blog</h4>
-        <p class="mb-0">Ehilà curiosa o curioso!<br><!-- comment --> 
+        <p class="mb-0">Ehilà!<br><!-- comment --> 
             Sono Chiara e sono molto felice tu sia su questo blog.<br><!-- comment -->
             Qui riporto i miei pensieri su quello che leggo, carico schede di aiuto per chi studia le lingue e materiale
             creativo per i bambini. 
---
+++ views/articles/tabella-frasi-uso-quotidiano-italiano-ucraino.php
@@ -0,0 +1,69 @@
+<div class="container">
+
+    <div class="row mb-5">
+        <div class="col justify-text">
+            <h2> Tabella con frasi di uso quotidiano italiano - ucraino </h2><br>
+
+            <div class="col-8 offset-2 text-center">
+                <img class="img-fluid" 
+                     alt="Anteprima tabella" 
+                     title="Tabella frasi 1"
+                     src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/quotidiano.jpg">
+                <figcaption>  
+
+                    <p>Anteprima della tabella da scaricare. </p>  
+
+                </figcaption> 
+            </div>
+        </div> 
+    </div>
+
+    <div>
+
+
+        <h2> Alcune espressioni utili </h2>
+        
+        
+        
+        Dopo aver visto 
+        <a class="article-title-link" href="<?php echo $GLOBALS['base_complete_url']; ?>/article/alfabeto-italiano-ucraino-tabella" target="_blank">
+            <b>l'alfabeto italiano-ucraino </b></a> 
+        ed essere passati al lessico, con ad esempio <a class="article-title-link" href="<?php echo $GLOBALS['base_complete_url']; ?>/article/giorni-settimana-ucraino-italiano" target="_blank">
+            <b>i giorni della settimana </b></a> e 
+        <a class="article-title-link" href="<?php echo $GLOBALS['base_complete_url']; ?>/article/mesi-anno-italiano-ucraino-tabella" target="_blank">
+            <b> i mesi dell'anno </b></a>,
+        
+        questa volta potete scaricare una tabella con alcune espressioni di uso quotidiano. <br><br>
+        
+        
+        Queste schede sono pensate per aiutare persone ucraine che si trovano a dover imparare l'italiano 
+        ed è per questo che ho aggiunto la pronuncia 
+        italiana agevolata tramite il cirillico e non tramite l'alfabeto fonetico internazionale che non tutti conoscono. <br><br>
+       
+        
+        Per chi invece è italiano o italiana e studia l'ucraino può essere interessante notare alcune cose:
+        <ul>
+        <li>il "Ciao" utilizzato quando ci si incontra e cioè "Привіт" diverso da quello usato quando ci si lascia che è "Бувай" 
+            (ricorda come suono il bye inglese). </li>
+        <li>la richiesta di aiuto "Допоможіть мені" che letteralmente sarebbe "aiutami" e che ho tradotto con un "Puoi aiutarmi?"
+            più simile a quello che verrebbe detto in italiano per rendere lo stesso senso.</li>
+        </ul>
+        
+        <br><br><!-- comment -->
+        Scrivetemi pure nei commenti se volete più materiale come questo e cosa potrebbe esservi utile.
+        
+        
+        
+ 
+        <b>Per scaricare la tabella potete cliccare sul link qui di seguito </b>--->
+        <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/frasiitaliano-ucraino.rar"> QUI</a>.<br><br>
+
+       
+        Buono studio!
+
+
+        <br><br>
+
+
+    </div>
+</div>
