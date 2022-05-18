76df6c6d34cc720f68a271ab0ac686a353475bda
Chiara Censorio <chiaracens@gmail.com>
2022-05-18 19:48:41 +0200

    Piccolissime modifiche

---
+++ views/articles/verbi-italiano-ucraino-vivere-presente-indicativo.php
@@ -0,0 +1,50 @@
+<div class="container">
+    <div class="row mb-4">
+        <div class="col-8 offset-2 text-center">
+            <img class="img-fluid" 
+                 alt="Anteprima tabella" 
+                 title="Tabella verbo Vivere ucraino italiano"
+                 src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/presentevivere.jpg">
+            <figcaption>  
+
+                <p>Anteprima della tabella da scaricare. </p>  
+
+            </figcaption> 
+        </div>
+
+    </div> 
+
+    <div class="row">
+        <div class="col justify-text">
+
+            <div>
+                <b>Bи можете завантажити файл </b>---> <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/vivere-ind-pres.rar"> тут. </a><br><br>
+                <b>Potete scaricare il file </b>---> <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/vivere-ind-pres.rar"> QUI. </a><br><br>
+
+                <br><br>
+                Continua la serie di materiali con lessico italiano e ucraino dopo la tabella sul presente indicativo del verbo "Potere" in ucraino: 
+                
+                <a  href="https://www.papergirlblog.com/article/verbi-italiano-ucraino-tabella-verbo-potere" target="_blank">
+                    presente indicativo verbo "Potere". </a>
+                <br><!-- comment -->
+                Questa volta una tabella sui verbi, in particolare il verbo "Vivere" al presente dell'indicativo.<br>
+                Queste tabelle sono ottime da avere sottomano per provare a creare piccole frasi e iniziare a utilizzare i verbi. Ne inserirò altre a breve. 
+
+                           <br>
+                Per la pronuncia vi invito a cliccare sull'articolo con la tabella dell'alfabeto illustrato (che potete trovare qui -->
+                <a  href="https://www.papergirlblog.com/article/alfabeto-italiano-ucraino-tabella" target="_blank">
+                    Tabella alfabeto italiano-ucraino - італійський алфавіт. </a> <br>
+                Condivido tutto qui, nella speranza che questi materiali possano aiutare qualcuno e 
+                sono scaricabili gratuitamente.
+               
+                <br><br>
+
+                Па-па!
+                <br><br>
+
+
+            </div>
+        </div>
+
+    </div>
+    </div>
