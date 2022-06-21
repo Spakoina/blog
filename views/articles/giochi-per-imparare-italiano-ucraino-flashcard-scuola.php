69aee8c84d346063246616f7ffccd3416df6d465
Chiara Censorio <chiaracens@gmail.com>
2022-06-21 10:25:22 +0200

    flashcardsucraino

---
+++ views/articles/giochi-per-imparare-italiano-ucraino-flashcard-scuola.php
@@ -0,0 +1,87 @@
+<div class="container">
+    <div class="row mb-4">
+        <div class="col-6 offset-3 text-center">
+            <img class="img-fluid" 
+                 alt="Anteprima tabella" 
+                 title="Anteprima tabella flashcards"
+                 src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/flashcardscuola.jpg">
+            <figcaption>  
+
+                <p>Anteprima delle flashcards da scaricare. </p>  
+
+            </figcaption> 
+        </div>
+
+    </div> 
+
+    <div class="row">
+        <div class="col justify-text">
+
+            <div>
+                
+                Dopo aver pubblicato qui l'alfabeto che avevo realizzato per mia madre e dopo la tabella sul  
+                <a  href="<?php echo $GLOBALS['base_complete_url']; ?>/article/verbi-italiano-ucraino-vivere-presente-indicativo" target="_blank">
+                    presente indicativo del verbo "Vivere" in ucraino</a>, questa volta vi propongo 
+                    un gioco che sicuramente aiuterà la memorizzazione del lessico italiano che riguarda il mondo della scuola. <br><!-- comment -->
+
+                    <br><!-- comment -->
+                    <h2> Cosa sono le flashcards?</h2><br>
+                    Devo ammetterlo, sono un po' ossessionata con l'apprendimento di nuovo lessico di lingue straniere.
+                Un modo molto divertente è quello del gioco tramite le flashcards: sono comode da portare dietro, per passare il tempo,
+                per ripassare i termini appena studiati o anche da regalare.<br><br><!-- comment -->
+                
+              
+                Le flashcards sono delle carte che <b>si usano per consolidare delle informazioni</b> e nello studio di una lingua
+                sono particolarmente utili per studiare il lessico. 
+                Da un lato abbiamo un disegno e dall'altro la parola da ricordare.<br><br>
+
+                Quando me le creo da sola di solito mi affido a Canva perché è semplice da usare e perché lì ho già la mia griglia pronta e
+                queste in particolare riguardano il lessico del mondo della scuola: da un lato ho inserito il termine in ucraino e dall'altro
+                la pronuncia della parola in italiano, letta tramite l'alfabeto 
+                ucraino, mentre l'accento delle parole cade sulle lettere in grassetto. 
+                Per un aiuto ulteriore con la pronuncia vi invito a cliccare sull'articolo con la
+
+                <a  href="<?php echo $GLOBALS['base_complete_url']; ?>/article/alfabeto-italiano-ucraino-tabella" target="_blank">
+                    tabella dell'alfabeto italiano-ucraino - італійський алфавіт.</a>
+
+<br><br>
+<h2>Come tagliare le flashcards</h2><br>
+                Per preparare le flashcards basterà tagliarle con una taglierina per carta (per fare prima), 
+                o con le forbici lungo le linee rosse e piegare poi il foglietto dela singola parola lungo
+                la linea verde. <br>
+                
+
+                <div class="row mb-4 mt-4">
+                    <div class="col-6 offset-3 text-center">
+                        <img class="img-fluid" 
+                             alt="Anteprima tabella" 
+                             title="Anteprima tabella flashcards"
+                             src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/cometagliare.jpg">
+                        <figcaption>  
+
+                            <p>Tagliare le cards è molto semplice </p>  
+
+                        </figcaption> 
+                    </div>
+
+                </div> 
+
+                
+                Potete incollare i due lati che si formeranno: in questo modo avrete da un lato il disegno con la parola in ucraino e dall'altro 
+                la traduzione in italiano con la pronuncia.<br><br><!-- comment -->
+                Bene, non vi resta che scaricare il file!
+                
+             <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/flashcards_itaucr_scuola.rar"> File da scaricare con flashcards</a>
+
+
+
+
+                (Bи можете завантажити файл <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/flashcards_itaucr_scuola.rar"> тут. </a>)<br><br>
+                
+                Buono studio! Па-па!
+
+            </div>
+        </div>
+
+    </div>
+</div>
