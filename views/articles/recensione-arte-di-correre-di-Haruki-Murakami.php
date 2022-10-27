c630730805a59f79f2463c21f8881b53842037d1
Chiara Censorio <chiaracens@gmail.com>
2022-10-27 17:57:33 +0200

    Articolo Murakami Arte di correre

---
+++ views/articles/recensione-arte-di-correre-di-Haruki-Murakami.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row mb-4">
+        <div class="col-6 offset-3 text-center">
+            <img class="img-fluid" 
+                 alt="Foto copertina libro Murakami" 
+                 title="Copertina libro L'arte di correre"
+                 src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/murakami-sm.jpg">
+            
+        </div>
+
+    </div> 
+
+    <div class="row">
+        <div class="col justify-text">
+
+            <div>
+                Cosa c'è nella mente di un uomo che ha avuto risultati eccellenti in entrambe le attività, in quella di corridore e in quella di scrittore?<br><br>
+
+                Bisogna solo iniziare a correre e lasciarsi guidare da Murakami: in sottofondo <b>musica rock</b> (quella che secondo lui più si addice alla corsa), 
+                i suoi pensieri che prendono forma, una pausa ogni tanto per riflettere sul <b>senso della solitudine</b>, su cosa significhi essere scrittore, sulla <b>forza di volontà</b>.<br><br>
+
+                Durante la corsa magari imboccare una strada sbagliata e cercare allora di "ritrovare le orme della strada percorsa".<br>
+
+                E infine fermarsi: riprendere fiato e tirare le somme.<br><br>
+                C'è <b>talento, concentrazione, metodicità, coerenza e poi perseveranza e umiltà</b>.<br>
+
+                Grazie Haruki Murakami.<br><br>
+
+Conosci Murakami? Avendo letto i suoi libri non l'avrei immaginato così metodico e disciplinato. Per me è stata una scoperta.
+
+
+            </div>
+        </div>
+
+    </div>
+</div>
