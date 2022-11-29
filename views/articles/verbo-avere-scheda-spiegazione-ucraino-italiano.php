431d59e44108889d597bb071da50701043d4c3fb
Chiara Censorio <chiaracens@gmail.com>
2022-11-29 22:04:15 +0100

    "Avere" ucraino

---
+++ views/articles/verbo-avere-scheda-spiegazione-ucraino-italiano.php
@@ -0,0 +1,55 @@
+<div class="container">
+
+    <div class="row mb-5">
+        <div class="col justify-text">
+            
+
+            <div class="col-8 offset-2">
+                <img class="img-fluid" 
+                     alt="Anteprima scheda" 
+                     title="Scheda verbo Avere"
+                     src="<?php echo $GLOBALS['base_complete_url']; ?>/img/imgarticles/avere_ucraino.jpg">
+                <figcaption>  
+
+                    <p>Anteprima della scheda da scaricare. </p>  
+
+                </figcaption> 
+            </div>
+        </div> 
+    </div>
+
+    <div class="row">
+        <div class="col justify-text">
+        <b>Завантажте файли </b>---> <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/avere_ucraino.rar"> тут </a>
+        
+        <h2> Cosa puoi trovare nella scheda scaricabile?</h2>
+
+        Questi materiali che puoi trovare sul sito sono nati con l'intento di <b>aiutare chi ha urgenza di imparare l'italiano dall'ucraino</b>. 
+        Oggi in questa pagina puoi trovare una scheda scaricabile su uno dei verbi più usati della lingua italiana e cioè il verbo "Avere".<br><!-- comment -->
+        Serve a esprimere l'idea del possesso sia di cose materiali che astratte.
+        <br><br>
+        In ucraino <b>esistono due strutture</b> che accompagnano il verbo "Avere".<br><br>
+        
+        
+        <font color="green">La prima</font> è più utilizzata nella parte occidentale dell'Ucraina ed è molto semplice: 
+        abbiamo la 
+        <font color="green">coniugazione del verbo</font>, che è quella che puoi trovare nella prima tabella, alla quale va aggiunto il 
+        <font color="green">complemento oggetto all'Accusativo.</font><br><br>
+        
+        
+        <font color="red">La seconda</font> è quella più comune in Ucraina e si forma con la preposizione
+        <font color="red">У</font>, insieme al <font color="red">sostantivo al caso Genitivo</font>  + <font color="red">є </font>.<br><br>
+        
+        Per la corretta lettura dei verbi guarda <a  href="<?php echo $GLOBALS['base_complete_url']; ?>article/alfabeto-italiano-ucraino-tabella" target="_blank">
+            l'alfabeto creato appositamente</a> e ti invito a guardare anche la  <a href="<?php echo $GLOBALS['base_complete_url']; ?>/materiale/essere_ucraino.rar" class="start-download"> 
+            tabella creata per l'altro ausiliare "Essere"</a>.
+        <br><br>
+       
+            Scrivimi se questo articolo ti è stato di aiuto e quali altri materiali vorresti vedere su questo sito.<br><br> Buono studio!
+
+        <br><br>
+
+
+    </div></div>
+</div>
+
